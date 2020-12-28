88279ac8292a0c13240bcb19a53f6e76ebca779e
Jordan Gillard <jordan-gillard@outlook.com>
2020-12-27 22:03:02 -0600

    feat: Removes unnecessary comments and code

--- flutter_project/lib/pages/home.dart
+++ flutter_project/lib/pages/home.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // is not restarted.
         primarySwatch: Colors.lightGreen,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Home Page'),
     );
   }
 }
@@ -25,69 +23,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text("Home"),
-          leading: IconButton(
-            icon: Icon(Icons.menu),
+        title: Text("Home"),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
             onPressed: () {},
           ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(Icons.more_vert),
-              onPressed: () {},
-            )
-          ],
-          flexibleSpace: SafeArea(
-            child: Icon(
-              Icons.photo_camera,
-              size: 75.0,
-              color: Colors.white70,
-            ),
-          ),
-          bottom: PreferredSize(
-            child: Container(
-              color: Colors.lightGreen.shade100,
-              height: 75.0,
-              width: double.infinity,
-              child: Center(
-                child: Text('Bottom'),
-              ),
-            ),
-            preferredSize: Size.fromHeight(75.0),
-          )),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          )
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
